1e71844f55f2142ff351150a30b3c3f9c79a6593
ArtavazdGevorgyan <ag15012003@gmail.com>
2023-03-23 17:19:22 +0400

    Update validate.py

--- validate.py
+++ validate.py
@@ -1,38 +1,40 @@
 import re
 
 
-def isValidEmail(email):
-    pattern = "^\w+[_.-]?\w+[@]+[a-z]+[.]\w{2,3}$"
-    if re.search(pattern, email) or email == '':
-        return True
-    return False
-
-
-def isValidName(name):
-    if len(name) > 30:
+class Validatior:
+    @staticmethod
+    def isValidEmail(email):
+        pattern = "^\w+[_.-]?\w+[@]+[a-z]+[.]\w{2,3}$"
+        if re.search(pattern, email):
+            return True
         return False
-    elif re.fullmatch("[A-Za-z ]+", name):
-        return True
-    return False
 
-
-def isValidSurname(surname):
-    if len(surname) > 30:
+    @staticmethod
+    def isValidName(name):
+        if len(name) > 30:
+            return False
+        elif re.fullmatch("[A-Za-z ]+", name):
+            return True
         return False
-    elif re.fullmatch("[A-Za-z ]+", surname) or surname == '':
-        return True
-    return False
 
+    @staticmethod
+    def isValidSurname(surname):
+        if len(surname) > 30:
+            return False
+        elif re.fullmatch("[A-Za-z ]+", surname) or surname == '':
+            return True
+        return False
 
-def isValidGender(gender):
-    if gender.strip() in ('male', 'female', ''):
-        return True
-    return False
+    @staticmethod
+    def isValidGender(gender):
+        if gender.strip() in ('male', 'female', ''):
+            return True
+        return False
 
-
-def isValidPhone(number):
-    digits = number[4:]
-    if number.startswith("+374") and len(number) == 12 \
-        and digits.isdigit() and digits[0] in '9743':
-        return True
-    return False
+    @staticmethod
+    def isValidPhone(number):
+        digits = number[4:]
+        if number.startswith("+374") and len(number) == 12 \
+                and digits.isdigit() and digits[0] in '9743':
+            return True
+        return False
