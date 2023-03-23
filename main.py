3118d7448f7be84b4b1bbdffd9e2117d4f1632e5
ArtavazdGevorgyan <ag15012003@gmail.com>
2023-03-23 17:17:24 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import funcs
-import sql_connection
 
-connection = sql_connection.create_connection('contact management/info.db')
-sql_connection.create_table(connection)
+funcs.create_DB('info.db')
+
 
 print("This is a contact management system")
 print("Every contact has:")
 print(f"\t{'Name' :<20}: no longet than 30 letters and can't be empty")
 print(f"\t{'Surname' :<20}: no longet than 30 letters or could be empty")
-print(f"\t{'Email' :<20}: 'exapmle@anymail.com' or could be emply")
-print(f"\t{'Phone Number' :<20}: '+374 -- --- ---' and can't be empty")
+print(f"\t{'Email' :<20}: 'exapmle@anymail.com' and can't be empty")
+print(f"\t{'Phone Number' :<20}: '+37490000000' and can't be empty")
 print(f"\t{'Gender' :<20}: male/female or could be emply\n")
 
 print("There are several commands you can use:")
@@ -29,8 +28,8 @@ command_list = {'add contact': funcs.add,
 
 while command != "exit":
     if command in command_list:
-        command_list[command](connection)
+        command_list[command]()
     else:
         print("Command not found\n Enter correct command i.e. \
-    (add contact, get contact, delete contact, view contacts list, exit)")
+    (add contact, get contact, delete contact, view contacts, exit)")
     command = input("Enter the command: ")
