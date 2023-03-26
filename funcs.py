594c4350f5aa6f6d9497e832dec33c420d57d69a
ArtavazdGevorgyan <ag15012003@gmail.com>
2023-03-26 17:52:20 +0400

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -4,6 +4,11 @@ from sql_connection import DB
 db = None
 
 
+def repr(contacts):
+    for contact in contacts:
+        print(contact)
+
+
 def create_DB(path):
     global db
     db = DB(path)
@@ -35,23 +40,47 @@ def add():
         print("Wrong Geneder format")
         gender = input(f"\t{'Enter the Gender':<25}:")
 
-    query = f"""
-    INSERT INTO contacts (name,surname,email,phone,gender)
-    VALUES ('{name}','{surname if surname != '' else 'NULL'}','{email}','{phone}','{gender if gender != '' else 'NULL'}')
-    """
+    if email:
+        query = f"""
+        INSERT INTO contacts (name,surname,email,phone,gender)
+        VALUES ('{name}','{surname if surname else 'NULL'}','{email if email else 'NULL'}','{phone}','{gender if gender else 'NULL'}')
+        """
+    else:
+        query = f"""
+        INSERT INTO contacts (name,surname,email,phone,gender)
+        VALUES ('{name}','{surname if surname else 'NULL'}',NULL,'{phone}','{gender if gender else 'NULL'}')
+        """
     db.execute_query(query)
 
 
 def get():
-    print(db.execute_read_query(f"""SELECT * 
+    clause = ''
+    tmpdict = {'name': input(f"\t{'Enter the Name':<25}:"),
+               'surname': input(f"\t{'Enter the Surname':<25}:"),
+               'email': input(f"\t{'Enter the Email':<25}:"),
+               'phone': input(f"\t{'Enter the Phone Number':<25}:"),
+               'gender': input(f"\t{'Enter the Gender':<25}:")}
+    for i, val in tmpdict.items():
+        if val:
+            clause += f"{i}='{val}' AND "
+    contact_list = db.execute_read_query(f"""SELECT * 
                                     FROM contacts
-                                    WHERE """))
-    pass
+                                    WHERE {clause}TRUE""")
+    if contact_list:
+        repr(contact_list)
+    else:
+        print("No such contact")
 
 
 def delete():
-    pass
+    print("If you dont know the 'id' of the contact press 'Enter'\n\
+        Find it with 'get contact' and then delete it")
+    id = input("Enter the id: ")
+    if not id:
+        query = f"""DELETE FROM contacts
+                    WHERE id = '{id}'"""
+        db.execute_query(query)
 
 
 def view():
-    print(db.execute_read_query("""SELECT * FROM contacts"""))
+    repr(db.execute_read_query("""SELECT * FROM contacts"""))
